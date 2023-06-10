8455677a5f1679e2aa86c4afaa3fc000ff800b53
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-06-11 00:04:47 +0530

    Added search functionality and search feed page

--- src/components/SearchFeed.js
+++ src/components/SearchFeed.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-    return (
-        <div>
+    const [videos, setVideos] = useState([]);
+    const { searchTerm } = useParams()
 
-        </div>
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${ searchTerm }`)
+            .then((data) => {
+                setVideos(data.items)
+            })
+    }, [searchTerm]);
+
+
+    return (
+        <Box p = { 4 } sx = { { overflowY: "auto", height: "90vh", flex: 2 } }>
+            <Typography variant = "h4" fontWeight = "bold" mb = { 2 } sx = { { color: "white" } }>
+                Search Results For: <span style = { { color: "#F31503" } }> { searchTerm } </span>
+            </Typography>
+            <Videos videos = { videos }/>
+        </Box>
     );
 };
 
