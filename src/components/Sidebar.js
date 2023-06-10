2a300ec88b29dcab76921975d68722e4303d6090
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-06-11 00:04:12 +0530

    Added channel detail page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,32 +1,36 @@
 import { categories } from "../utils/constants";
 import { Stack } from "@mui/material";
 
-const Sidebar = ({selectedCategory, setSelectedCategory}) => {
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
     return (
         <Stack
             direction = "row"
             sx = { {
                 overflowY: "auto",
+                width: { md: "200px" },
                 height: { sx: "auto", md: "95%" },
                 flexDirection: { md: "column" },
             } }
         >
-            {categories.map((category) => (
+            { categories.map((category) => (
                 <button
-                    className="category-btn"
-                    onClick={() => setSelectedCategory(category.name)}
-                    style={{
+                    className = "category-btn"
+                    onClick = { () => setSelectedCategory(category.name) }
+                    style = { {
                         background: category.name === selectedCategory && "#FC1503", color: "white"
-                    }}
+                    } }
                 >
-                    <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
-                        {category.icon}
+                    <span style = { {
+                        color: category.name === selectedCategory ? "white" : "red",
+                        marginRight: "15px"
+                    } }>
+                        { category.icon }
                     </span>
-                    <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
-                        {category.name}
+                    <span style = { { opacity: category.name === selectedCategory ? "1" : "0.8" } }>
+                        { category.name }
                     </span>
                 </button>
-            ))}
+            )) }
         </Stack>
     )
 };
